c3a504b744e44aaebf55d5861301a07892956af5
Jamie Bibat <jamie.bibat2017@gmail.com>
2018-06-07 15:26:17 +0800

    Initials Commit

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Service</strong> by <a href="#">Jamie Bibat</a>. 
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ resources/assets/js/components/Myheader.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link to="/phonebook" class="navbar-item"><b>Phonebook</b></router-link>
+  		<div class="navbar-menu is-active">
+        <router-link to="/home" class="navbar-item">Home</router-link>
+        <router-link to="/about" class="navbar-item">About</router-link>
+  		</div>
+  	</div>
+  </nav>
+</template>
